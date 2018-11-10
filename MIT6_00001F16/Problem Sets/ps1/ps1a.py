82329becd0e07ee7078053a0b80f66c2dc53d0f9
kriselle <kriselle@askwonder.com>
2018-11-10 11:49:21 -0500

    ps1a solution

---
+++ MIT6_00001F16/Problem Sets/ps1/ps1a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 10 11:45:04 2018
+
+@author: kriselle
+"""
+
+annual_salary = int(input('Enter your starting annual salary: '))
+portion_saved = float(input('Enter the percent of your salary to be saved as a decimal: '))
+monthly_portion_saved = (annual_salary / 12.0) * portion_saved
+
+total_cost = int(input('Enter cost of your dream home: '))
+portion_down_payment = .25 * total_cost
+
+current_savings = 0.0
+r = .04
+months = 0
+
+while current_savings < portion_down_payment:
+    months += 1 # += 1 adds 1 to the value and assigns that value to the result sum  
+    monthly_return = current_savings * (r/12)
+    current_savings += monthly_return + monthly_portion_saved
+
+print("Number of months: ", months)
+
